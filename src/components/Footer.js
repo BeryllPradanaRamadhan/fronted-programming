5d1a5ed645f2db0ae1407af44a4a8a7da9aaed36
beryllpradanaramadhan17@gmail.com <you@example.com>
2023-03-21 16:26:15 +0700

    benerin file yang salah

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 function Footer() {
     return (
-      <Footer>
+      <footer>
         <h2>Copyright @Beryll</h2>
         <p>Created by React.js</p>
-      </Footer>
+      </footer>
     );
   }
 
