2ef750200bcde1ab505427bb7620622c4f2caa3b
beryllpradanaramadhan17@gmail.com <you@example.com>
2023-04-06 15:25:09 +0700

    Pertemuan-6

--- src/components/AddMovieForm/AddMovieForm.js
+++ src/components/AddMovieForm/AddMovieForm.js
@@ -3,18 +3,18 @@ import styles from "./AddMovieForm.module.css";
 function AddMoviesForm() {
     return (
         <div className={styles.container}>
-           <section className={styles.AddMoviesForm}>
-            <div className={styles.AddMoviesForm__left}>
-                <img className={styles.AddMoviesForm__image} src="https:///picsum.photos/600/400" alt="placeholder" />
+           <section className={styles.AddMovieForm}>
+            <div className={styles.AddMovieForm__left}>
+                <img className={styles.AddMovieForm__image} src="https:///picsum.photos/600/400" alt="placeholder" />
             </div>
-            <div className={styles.AddMoviesForm__right}>
+            <div className={styles.AddMovieForm__right}>
                 <h2 className={styles.title1}>Add Movie</h2>
             <form >
                 <div className={styles.title}>
                     <label htmlFor="title" className={styles.labeltitle}>Title</label> <br />
                     <input type="text" id="title" name="title" className={styles.inputtitle}/>
                 </div>
-                <div>
+                <div className={styles.year}>
                      <label htmlFor="year" className={styles.labelyear1}>Year</label> <br />
                      <input type="text" id="year" name="year" className={styles.inputyear}/>
                 </div>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,86 +1,49 @@
-import styles from "./Movies.module.css"
+import Movie from "../Movie/Movie";
+import styles from "./Movies.module.css";
+//import data movies
+import data from "../utils/data";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function Movies() {
-    return(
-        <div className={styles.container}>
-            <section className={styles.movies}>
-              <h2 className={styles.movies__title}>Latest Movies</h2>
-             <div className={styles.movie__container}>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-                <div className={styles.movie}>
-                    <img className={styles.movie__image} src="https://picsum.photos/300/400" 
-                    alt="" 
-                    />
-                    <h3 className={styles.movie__title}>Title Movie</h3>
-                    <p className={styles.movie__date}>2022</p>
-                </div>
-             </div>
-            </section>
+    // Membuat variabe movies
+    const [movies, setMovies] = useState(data);
+
+
+//   membuat fungsi tambah film
+// di jalankan ketika tombol di clik
+function tambahFilm() {
+    const movie  = {
+    id: nanoid(4),
+    title: "Spiral Jigsaw",
+    year: "2021",
+    type: "Movie",
+    poster: "https://picsum.photos/300/400",
+};
+    // Menambahkan movie ke state movies
+    // spread operator: copy and merge array
+    setMovies([...movies, movie])
+}
+  return (
+    <div className={styles.container}>
+      <section className={styles.movies}>
+        <h2 className={styles.movies__title}>Latest Movies</h2>
+        <div className={styles.movie__container}>
+          {/* 
+          * Lopping data movies: map.
+          * Render Component Movie
+          * Kirim props movie
+          */}
+          {movies.map(function (movie) {
+            return <Movie key={movie.id} movie={movie} />;
+          })}
         </div>
-    );
+        
+        {/* Menambahkan event on Click */}
+        <button onClick={tambahFilm}>Tambah Film</button>
+      </section>
+    </div>
+  );
 }
 
-export default Movies
-
+export default Movies;
